1ecfee59f25faaefff20b36281a6bf59e5e8156e
phroggyy <leo.sjoberg@gmail.com>
2015-02-25 12:42:32 +0100

    initial commit

---
+++ Deck.hs
@@ -0,0 +1,93 @@
+module Deck 
+  ( Card
+  , possiblePoints
+  , Deck
+  , DeckS
+  , cards
+  , gen
+  , mkDeck
+  , draw
+  , shuffle
+  , takeRandomCard
+  , takeCardAt ) where
+
+import System.Random
+import Control.Monad.State
+
+-- |The 'Card' data type represents the possible playing card ranks.
+data Card = Ace
+          | Two
+          | Three
+          | Four
+          | Five
+          | Six
+          | Seven
+          | Eight
+          | Nine
+          | Ten
+          | Jack
+          | Queen
+          | King
+          deriving (Enum, Show)
+
+
+-- possibleValues 
+possibleValues :: Card -> [Int]
+possibleValues card
+    | card == Ace = [1, 11]
+    | card `elem` [Jack,Queen,King] = [10]
+    | otherwise = fromEnum card
+
+
+-- |'possiblePoints' calculates all possible scores for a given hand.
+possiblePoints :: [Card] -> [Int]
+possiblePoints hand = nub $ map sum $ mapM possibleValues hand
+
+
+-- |The 'Deck' data type represents a deck of cards that can be shuffled.
+data Deck = Deck
+  { cards :: [Card]
+  , gen :: StdGen }
+  deriving (Show)
+
+-- |'mkDeck' will construct a 52-card deck.
+mkDeck :: StdGen -> Deck
+mkDeck g = 
+  Deck { cards = [ card | card <- [Ace ..], _ <- [1..4] :: [Int] ]
+       , gen = g }
+
+-- |The 'DeckS' is the current state of the 'Deck'.
+type DeckS a = State Deck a
+
+-- |'draw' will take one card off the top of the deck.
+draw :: DeckS Card
+draw = takeCardAt 0
+
+-- |'shuffle' takes a 52-card deck and randomly shuffles its elements.
+shuffle :: DeckS ()
+shuffle = do
+  curr <- get
+  shuffled <- replicateM 52 takeRandomCard
+  put curr { cards = shuffled }
+
+-- |'takeRandomCard' will pick one random card from the deck and remove it.
+-- It is a helper-function used by 'shuffle'.
+takeRandomCard :: DeckS Card
+takeRandomCard = do
+  curr <- get
+  let n = length $ cards curr
+      (i, gen') = randomR (0, n) $ gen curr
+  card <- takeCardAt i
+  put curr { gen = gen' }
+  return card
+
+-- |'takeCardAt' will pick the card at the given index and remove it from the
+-- deck.
+takeCardAt :: Int -> DeckS Card
+takeCardAt i = do
+  curr <- get
+  let (cards', cards'') = splitAt (i + 1) $ cards curr
+      card              = last cards'
+      newCards          = init cards' ++ cards''
+  put curr { cards = newCards }
+  return card
---
+++ main.hs
@@ -0,0 +1,128 @@
+module Main where
+
+import Deck
+
+import Data.List
+import System.Random
+import Control.Monad.State
+import Control.Monad.Operational
+import qualified Data.Map.Strict as M
+
+data PlayerType = Dealer | Player
+                deriving (Ord, Eq)
+
+data GameActions a where
+    GetAction :: PlayerType -> GameActions Action
+    ShowState :: GameActions ()
+    Win :: GameActions ()
+    Lose :: GameActions ()
+
+type GameS = ProgramT GameActions (State Game)
+
+main :: IO ()
+main = do
+  stdGen <- getStdGen
+  interpretIO M.empty (mkGame stdGen) gameLoop
+
+interpretIO :: M.Map PlayerType (Game -> IO Action) -> Game -> GameS a -> IO a
+interpretIO strats s instr = case runState (viewT instr) s of
+                          (a, ns) -> evalinstr ns a 
+    where
+        evalinstr   _ (Return x) = return x 
+        evalinstr stt (a :>>= f) = 
+            let runC a' = interpretIO strats stt (f a')
+    in  case a of
+            GetAction pt ->
+                let strategy = M.findWithDefault (const (return Stay)) pt strats
+                in  strategy stt >>= runC
+            ShowState -> putStrLn "show state" >>= runC
+            Win -> putStrLn "You win" >>= runC
+            Lose -> putStrLn "You lose" >>= runC
+
+
+data Game = Game
+  { deck  :: Deck
+  , hands :: M.Map PlayerType [Card]
+  }
+
+data Action = Hit | Stay deriving (Eq, Read)
+
+runAction :: PlayerType -> GameS Action
+runAction pt = do
+    action <- singleton (GetAction pt)
+    when (action == Hit) $ do
+        curr <- get
+        let (card, deck') = runState draw $ deck curr
+        put curr { deck = deck'
+                 , hands = M.insertWith (++) pt [card] (hands curr)
+                 }
+    return action
+
+gameLoop :: GameS ()
+gameLoop = do
+  playerAction <- runAction Player
+  dealerAction <- runAction Dealer
+  pbust <- isBust Player
+  dbust <- isBust Dealer
+  let bothStayed = playerAction == Stay && dealerAction == Stay
+      gameOver = bothStayed || pbust || dbust
+  if gameOver
+      then handleGameOver
+      else gameLoop
+
+handleGameOver :: GameS ()
+handleGameOver = do
+    singleton ShowState
+    hs <- gets hands
+    let playerH = M.findWithDefault [] Player hs
+        dealerH = M.findWithDefault [] Dealer hs
+    singleton $ if won playerH dealerH then Win else Lose
+
+won :: [Card] -> [Card] -> Bool
+won playerH dealerH = playerScore > dealerScore
+  where playerScore = score playerH
+        dealerScore = score dealerH
+
+score :: [Card] -> Int
+score h
+  | bust h    = 0
+  | otherwise = best h
+
+isBust :: PlayerType -> GameS Bool
+isBust pt = gets (check . hands)
+    where
+        check h = case M.lookup pt h of
+                      Just cs -> bust cs
+                      Nothing -> True
+
+bust :: [Card] -> Bool
+bust = all (21 <) . possiblePoints
+
+twentyOne :: [Card] -> Bool
+twentyOne = elem 21 . possiblePoints
+
+best :: [Card] -> Int
+best = maximum . filter (21 >=) . possiblePoints
+
+showDealer :: [Card] -> String
+showDealer hand = "[" ++ show (head hand) ++ "," ++ intersperse ',' hidden ++ "]"
+  where n = length $ tail hand
+        hidden = replicate n '?'
+
+mkGame :: StdGen -> Game
+mkGame g = Game
+  { deck = d'
+  , hands = M.fromList [(Player, playerH), (Dealer, dealerH)]
+  }
+  where d = execState shuffle $ mkDeck g
+        ((playerH, dealerH), d') = runState deal d
+
+deal :: DeckS ([Card], [Card])
+deal = do
+  mine   <- draw
+  yours  <- draw
+  mine'  <- draw
+  yours' <- draw
+  let me = [mine, mine']
+      you = [yours, yours']
+  return (me, you)
